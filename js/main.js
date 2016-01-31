84aa2a65b525fe2168bb7b97f5363091ab8eba25
cornixxx <nastya.cornix@gmail.com>
2016-02-01 01:04:01 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -68,4 +68,15 @@ $(function () {
 });
 
 
+/* slider */
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlsContainer: $(".custom-controls-container"),
+        customDirectionNav: $(".custom-navigation a")
+    });
+});
+
+
 
